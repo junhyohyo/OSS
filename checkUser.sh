1bdd97e15240141481b366c07b0aedc292e21d4f
junhyohyo <kyu2718@naver.com>
2024-06-03 06:39:21 +0000

    modify checkUser

--- checkUser.sh
+++ checkUser.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-
-# 인수가 하나인지 확인합니다.
 if [ "$#" -ne 1 ]; then
   echo "인수가 하나가 아닙니다."
   exit 1
@@ -10,18 +8,15 @@ fi
 
 user=$1
 
-# 무한 루프 시작
 while true
 do
 
-  # 로그인한 유저 확인
   if who | grep -w "$user"
     then
     echo "$user 로그인함!" 
     exit 0
   fi
 
-  # 60초 대기
   sleep 60
 done
 
